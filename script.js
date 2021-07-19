9d2eae54984daf26a07e7d61b4171ddc6158f03e
Dylan Gallagher <1dylangallagher@gmail.com>
2021-07-19 09:21:58 +0100

    Added 'clear all' fuctionality

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 const container = document.getElementById("container");
+let divs;
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
         cell.innerText = (' ');
-        container.appendChild(cell).className = "grid-item";
+        container.appendChild(cell).className = "grid-item";            
+        
+        divs = document.querySelectorAll(".grid-item");
+        divs.forEach(function(elem) {
+            elem.addEventListener("mouseover", event => {
+             elem.setAttribute('class', 'hover');                              
+            });
+        });
     };
-  };
-  
-makeRows(32, 32);
+};
 
-let divs = document.querySelectorAll(".grid-item");
+makeRows(16,16);
 
-divs.forEach(function(elem) {
-    elem.addEventListener("mouseover", event => {
-        elem.setAttribute('class', 'hover');                              
+let btn = document.querySelector('#clear');
+btn.addEventListener('click', event => {
+    console.log('hello');
+    divs.forEach(function(elem) {
+        elem.removeAttribute('class');
     });
-});
+    size = prompt('What resolution canvas do you want?');
 
+    size = Number(size);
 
-divs.forEach(function(elem) {
-    elem.addEventListener("mouseout", event => {
-        elem.removeAttribute('class');                           
+    divs.forEach(function(elem) {
+        container.removeChild(elem);
     });
+    makeRows(size, size);
 });
